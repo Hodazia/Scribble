985f274b826a6ecf5c0c12450efc778c935fa7c8
Hodazia <zhoda315@gmail.com>
2025-08-07 18:36:08 +0530

    [feat] added common packages for zod and token

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,37 @@
 import express from "express";
+import { Request,Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs"
+import auth from "./middleware";
+import {CreateUserSchema }  from "@repo/common-zod/types"
 
 const app = express();
+const SALT_ROUNDS = 10;
+// 
+app.get("/" ,(req:Request,res:Response) => {
+    res.send("Hello world");
+})
+
+// DO ZOD VALIDATION IN BOTH OF THEM
+app.post("/signup", async (req:Request,res:Response) => {
+    const {email,name,password} = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    // this u store in the db
+
+
+    // insert it into the db
+
+
+})
+
+app.post("/signin", async (req:Request,res:Response) => {
+
+});
+
+app.post("/create-room", auth, async (req:Request,res:Response) => {
+    
+})
 
 app.listen(3000, () => {
     console.log("greetings u are connected now !");
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,36 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
 
+import {JWT_SECRET} from "@repo/backend-common/config"
+// we will have a global JWT_SECRET, in the packages folder
 // create a new web socket server with a port of 8080
 const wss = new  WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws,request) => {
     console.log("web socket connection is made ");
+    // extract the url of the website it will contain http://localhost:3000/?token=2342r
+
+    const url = request.url;
+    if(!url)
+    {
+        return
+    }
+
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+
+    const token = queryParams.get("token");
+    if (!token) throw new Error("Token not provided");
+
+    const decoded = jwt.verify(token, JWT_SECRET as string) as JwtPayload;
+    const userId = decoded.id;
+
+    if (!userId) 
+        {
+            //throw new Error("Invalid token")
+            ws.close();
+        ;}
 
     ws.on("message", (msg) => {
         console.log("Message received is ", msg);
