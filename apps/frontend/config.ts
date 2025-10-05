ae2f95d8598e57d984bfc093ca628a126cbc90b4
Hodazia <zhoda315@gmail.com>
2025-10-05 19:39:59 +0530

    [feat] vercel.json

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,4 +1,6 @@
 export const WS_BACKEND =
-    process.env.WS_BACKEND || "ws://localhost:8080";
+    process.env.NEXT_PUBLIC_WS_BACKEND || "wss://ws-scribble-backend.onrender.com";
+// ws:localhost:8081
 export const HTTP_BACKEND =
-    process.env.HTTP_BACKEND || "http://localhost:3001";
+    process.env.NEXT_PUBLIC_HTTP_BACKEND || "https://httpbackend-ygyz.onrender.com";
+// localhost:3000
